10a9b875da8b6c9fc7e0491d1416751f30852830
Tássio <62919250+bet-gif@users.noreply.github.com>
2025-11-04 23:23:53 +0000

    cor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,7 +34,7 @@ export default function Header() {
 
 // 💅 Estilos do Header
 const HeaderContainer = styled.header`
-  background-color: #1e293b;
+  background-color: #000000ff;
   color: white;
   padding: 1rem 2rem;
   position: fixed;
